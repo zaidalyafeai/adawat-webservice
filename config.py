53721c78d355dbe39b08c2f4fbffaa2defdbe9d5
Zaid Alyafeai <alyafey22@gmail.com>
2022-10-21 02:27:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 
 class Config:
-    REDIS_URL = environ.get('REDIS_URL', 'redis://default:bjp9MLfxGbaKtenObZEF@containers-us-west-63.railway.app:6531')
+    REDIS_URL = environ.get('REDIS_URL', 'redis://localhost:6531')
 
     HF_SECRET_KEY = environ.get('HF_SECRET_KEY')
 
